1fb7beb9076d0553b6d26f3c27cc0a40de22e5da
kot9pa <kot9paa@gmail.com>
2024-03-22 14:28:42 +0200

    init commit

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: en/ru/es/ko/fr etc.")
+
+@pytest.fixture(scope="class")
+def browser(request):
+    browser_name = request.config.getoption("browser")
+    user_language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
+        #options.add_experimental_option('excludeSwitches', ['enable-logging']) # Not work with headless
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        #options.add_argument("--headless=new") # Hide windows
+        options.add_argument("--log-level=3") # Hide console logs
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser should be chrome or firefox")
+    browser.implicitly_wait(10)
+    yield browser
+    browser.quit()
